77d0e7d0584ad8529d82c2ae57634aa28a64b309
Zakhar Tikhonov <erroraccount@bk.ru>
2025-12-12 09:19:25 +0400

    Data Receive Function

--- 2gis_main.py
+++ 2gis_main.py
@@ -1,24 +1,30 @@
-import time
+import time, re
 from playwright.sync_api import sync_playwright, Page
 from translate import Translator
 from typing import List
 
 
 class TwoGisMapParse:
-    def __init__(self, keyword: str, sity: str):
+    def __init__(self, keyword: str, sity: str, max_num_firm: int):
         self.keyword = keyword  #  Ищем по ключевому слову
         self.sity = sity  # Ищем в определённом городе
-        self.list_organizations_name = []  # Список для организаций
+        self.max_num_firm = max_num_firm  # Максимальное количество фирм
 
-    def extract_organization_links_current_page(self, page: Page) -> List[str]:
+    def eng_sity(self):
+        """Переводим город на английский для удобства"""
+        return self.translate_text(self.sity).lower()
+
+    def __get_links(self) -> List[str]:
         """Извлекаем ссылки на организации, находящиеся на странице"""
         print("Собираем ссылки на организации с текущей страницы...")
 
         links = []
         link_selector = 'a[href*="/firm/"]'
-
-        found_links = page.query_selector_all(link_selector)  # Ищем только видимые карточки организаций(firm)
-        for link in found_links:
+        
+        found_links = self.page.query_selector_all(link_selector)  # Ищем только видимые карточки организаций(firm)
+        for count, link in enumerate(found_links):
+            if count > self.max_num_firm:  # Делаем так, чтобы кол-во не превышало желамое кол-во объявлений
+                break 
             if not link.is_visible():  # Проверяем, видим ли элемент
                 continue
             href = (link.get_attribute("href") or "")  # Находим элемент на стр., где есть /firm/
@@ -26,11 +32,45 @@ class TwoGisMapParse:
             if (href and "/firm/" in href and self.eng_sity() in href):
                 href = rf"https://2gis.ru{href}"
                 links.append(href)
+                self.__get_firm_data(url=href)
         return links
 
-    def eng_sity(self):
-        """Переводим город на английский для удобства"""
-        return self.translate_text(self.sity).lower()
+    def __get_firm_data(self, url: str):
+        """Берем данные фирмы: название, телефон, сайт"""
+        self.page2 = self.context.new_page()  # Создаем новую страницу
+        self.page2.goto(url=url)  # Переходим на неё
+        
+        # Название фирмы
+        firm_title = self.page2.title().split(',')[0]  #Отделяем: (Назв.фирмы, ул. ...) 
+        print(firm_title)
+        
+        # Номер телефона
+        try:
+            # Находим контейнер, затем ссылку внутри него
+            phone_container = self.page2.query_selector(':has(button:has-text("Показать телефон")):has(a[href^="tel:"])')
+            if phone_container:
+                # Теперь ищем телефон внутри этого контейнера
+                phone = phone_container.query_selector('a[href^="tel:"]')
+                print(phone.get_attribute("href")[4:])  # Вывожу без tel:
+            else:
+                print("Контейнер с телефоном и кнопкой не найден")
+        except Exception as e:
+            print(f"Ошибка: {e}")
+        
+        # Название сайта
+        site_elements = self.page2.query_selector_all('a[href^="https://link.2gis.ru/"]')  # Ищем ссылки(сайт)
+        if site_elements:  # Если есть хоть одна ссылка
+            site_texts = [element.text_content().strip() for element in site_elements]
+            try:
+                a = list(filter(lambda i: i if ('.ru' in i or '.com' in i or '.net' in i or '.рф' in i) and
+                            '@' not in i else '', site_texts))  # Фильтруем, чтоб выводилось нужное 
+                print(f"{a[0]}")
+            except:
+                print(f"Нет ссылки на сайт")
+                
+        self.page2.close()
+
+# TODO: Сделать запись данных в xlsx
 
     def parse(self):
         """Парсинг сайта"""
@@ -43,9 +83,12 @@ class TwoGisMapParse:
             self.page.get_by_placeholder("Поиск в 2ГИС").type(text=self.keyword, delay=0.4)
             self.page.keyboard.press("Enter")  # Нажимаем Enter
             time.sleep(3)  # Задержка для загрузки страницы
-            print(self.extract_organization_links_current_page(self.page))  # Вывод ссылок на организации
-            self.page.click('[style="transform: rotate(-90deg);"]')  # Кликаем на кнопку перехода на след. страницу
-            time.sleep(120)
+            print(self.__get_links())  # Вывод ссылок на организации
+            # for i in range(6):
+            #     self.page.click('[style="transform: rotate(-90deg);"]')  # Кликаем на кнопку перехода на след. страницу
+            time.sleep(4)
+            self.__get_links()
+            time.sleep(180)
 
     def translate_text(self, text, from_lang="ru", to_lang="en"):
         # Создаем объект Translator, указывая исходный язык и язык перевода
@@ -60,4 +103,4 @@ class TwoGisMapParse:
 
 
 if __name__ == "__main__":
-    TwoGisMapParse("Мойка", "Ростов на Дону").parse()
+    TwoGisMapParse("Музеи", "Саратов", 322).parse()  # Ключевое слово, город, кол-во объявлений
